e6201a83d0ff2f9bef5f306de4452459c3880450
LefterisKarampas <mr.lef_21113@hotmail.com>
2017-11-09 16:05:13 +0200

    get_color_object

--- src/object_detected.py
+++ src/object_detected.py
@@ -3,11 +3,16 @@ import rospy
 import roslib
 from object_recognition_msgs.msg import RecognizedObjectArray
 from std_msgs.msg import String
+from camera_test.msg import coke
 import tf as tf
 
+pub = None
 
 def callback(data):
     i = 0
+    tf_cokes = coke()
+    x = []
+    pub = rospy.Publisher('coke_tf', coke, queue_size=10)
     while(i < len(data.objects)):
         print(data.objects[i].pose.pose.pose)
         br = tf.TransformBroadcaster()
@@ -20,12 +25,15 @@ def callback(data):
                         data.objects[i].pose.pose.pose.orientation.w),
                         rospy.Time.now(),
                         "/coke"+str(i+1),"/camera_rgb_optical_frame")
+        x = x+["coke"+str(i+1)]
         i = i+1
+    tf_cokes.tf_names = x
+    tf_cokes.size = i;
+    pub.publish(tf_cokes)
 
 def listener():
     rospy.init_node('object_detected', anonymous=True)
     rospy.Subscriber("recognized_object_array", RecognizedObjectArray, callback)
-
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
--- src/pcl.cpp
+++ src/pcl.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
 #include <iostream>
 // PCL specific includes
+#include <tf/transform_broadcaster.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
@@ -10,70 +13,97 @@
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
 #include <pcl_ros/transforms.h>
+#include <camera_test/coke.h>
+#include <sensor_msgs/PointCloud2.h>
 
 using namespace std;
+using namespace sensor_msgs;
+using namespace message_filters;
 ros::Publisher pub;
 
+bool flag = false;
+string * tf_frames;
+int size = 0;
+
+
 void cloud_cb (const pcl::PCLPointCloud2ConstPtr& input){
-
 	pcl::PCLPointCloud2::Ptr cloud_pass(new pcl::PCLPointCloud2);
+	if(flag){
+		pcl::PassThrough<pcl::PCLPointCloud2> pass_through_filter;
+		tf::TransformListener listener;
+		ros::Rate rate(10.0);
+		tf::StampedTransform transform;
+		for(int i =0;i<size;i++){
 
-	pcl::PassThrough<pcl::PCLPointCloud2> pass_through_filter;
-	tf::TransformListener listener;
-	ros::Rate rate(10.0);
-	tf::StampedTransform transform;
-	std::string temp = "/"+input->header.frame_id;
-	try{
-		listener.waitForTransform(input->header.frame_id,"/coke1", ros::Time(0),ros::Duration(3.0));
-		listener.lookupTransform(input->header.frame_id,"/coke1",ros::Time(), transform);
-		// Z
-		pass_through_filter.setInputCloud (input);
-		pass_through_filter.setFilterFieldName ("z");
-	  	pass_through_filter.setFilterLimits (transform.getOrigin().z()-0.05,transform.getOrigin().z()+0.05);
-	  	pass_through_filter.filter (*cloud_pass);
-	  	// X 
-	  	pass_through_filter.setInputCloud (cloud_pass);
-	  	pass_through_filter.setFilterFieldName ("x");
-	  	pass_through_filter.setFilterLimits (transform.getOrigin().x()-0.05,transform.getOrigin().x()+0.05);
-	  	pass_through_filter.filter (*cloud_pass);
-	  	// Y 
-	  	pass_through_filter.setInputCloud (cloud_pass);
-	  	pass_through_filter.setFilterFieldName ("y");
-	  	pass_through_filter.setFilterLimits (transform.getOrigin().y()-0.05,transform.getOrigin().y()+0.05);
-		pass_through_filter.filter (*cloud_pass);
-		pub.publish(*cloud_pass);
-		pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudXYZRGB(new pcl::PointCloud<pcl::PointXYZRGB>);
-   		pcl::fromPCLPointCloud2(*cloud_pass,*PointCloudXYZRGB);
-		int count = 0;
-		int r1 = 0,g1 =0,b1 = 0;
-		int cloudsize = (PointCloudXYZRGB->width) * (PointCloudXYZRGB->height);
-		for (int i=0; i< cloudsize; i++){
-			//std::cout << "(x,y,z,rgb) = " << PointCloudXYZRGB->points[i] << std::endl;
-			uint32_t rgb = *reinterpret_cast<int*>(&PointCloudXYZRGB->points[i].rgb);
-			uint8_t r = (rgb >> 16) & 0x0000ff;
-			uint8_t g = (rgb >> 8)  & 0x0000ff;
-			uint8_t b = (rgb)       & 0x0000ff;
-			r1+= r;
-			g1+= g;
-			b1+= b;
-			count++;
-		}
-		ROS_INFO("R G B:%d %d %d",r1/count,g1/count,b1/count);
-		if(r1/count > 180){
-			ROS_INFO("RED");
-		}
-		else if(b1/count > 160){
-			ROS_INFO("BLUE");
+			try{
+				listener.waitForTransform(input->header.frame_id,tf_frames[i], ros::Time(0),ros::Duration(3.0));
+				listener.lookupTransform(input->header.frame_id,tf_frames[i],ros::Time(), transform);
+				// Y 
+			  	/*pass_through_filter.setInputCloud (input);
+			  	pass_through_filter.setFilterFieldName ("y");
+			  	pass_through_filter.setFilterLimits (transform.getOrigin().y()-0.15,transform.getOrigin().y()+0.2);
+				pass_through_filter.filter (*cloud_pass);*/
+				// X 
+			  	pass_through_filter.setInputCloud (input);
+			  	pass_through_filter.setFilterFieldName ("x");
+			  	pass_through_filter.setFilterLimits (transform.getOrigin().x(),transform.getOrigin().x()+0.05);
+			  	pass_through_filter.filter (*cloud_pass);
+				// Z
+				pass_through_filter.setInputCloud (cloud_pass);
+				pass_through_filter.setFilterFieldName ("z");
+			  	pass_through_filter.setFilterLimits (transform.getOrigin().z()-0.03,transform.getOrigin().z());
+			  	pass_through_filter.filter (*cloud_pass);
+				pub.publish(*cloud_pass);
+				pcl::PointCloud<pcl::PointXYZRGB>::Ptr PointCloudXYZRGB(new pcl::PointCloud<pcl::PointXYZRGB>);
+		   		pcl::fromPCLPointCloud2(*cloud_pass,*PointCloudXYZRGB);
+				int count = 0;
+				int r1 = 0,g1 =0,b1 = 0;
+				int cloudsize = (PointCloudXYZRGB->width) * (PointCloudXYZRGB->height);
+				for (int i=0; i< cloudsize; i++){
+					//std::cout << "(x,y,z,rgb) = " << PointCloudXYZRGB->points[i] << std::endl;
+					uint32_t rgb = *reinterpret_cast<int*>(&PointCloudXYZRGB->points[i].rgb);
+					uint8_t r = (rgb >> 16) & 0x0000ff;
+					uint8_t g = (rgb >> 8)  & 0x0000ff;
+					uint8_t b = (rgb)       & 0x0000ff;
+					r1+= r;
+					g1+= g;
+					b1+= b;
+					count++;
+				}
+				if(count > 0){
+					r1 = r1/count;
+					g1 = g1/count;
+					b1 = b1/count;
+					ROS_INFO("Frame: %s -> R G B:%d %d %d",tf_frames[i].c_str(),r1,g1,b1);
+					static tf::TransformBroadcaster br;
+					string color;
+					if(b1 > 180){
+						br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), input->header.frame_id, "brown"));
+					}
+					else{
+						br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), input->header.frame_id, "red"));
+					}
+				}
+			}
+			catch (tf::TransformException ex){
+				ROS_ERROR("%s",ex.what());
+				ros::Duration(1.0).sleep();
+			}
 		}
 	}
-	catch (tf::TransformException ex){
-		ROS_ERROR("%s",ex.what());
-		ros::Duration(1.0).sleep();
-	}
-	//pass through filter
-	printf("%d - %d\n",cloud_pass->data.size(),input->data.size());
 }
 
+void frames(const camera_test::coke & frame){
+	flag = true;
+	ros::NodeHandle nh;
+	tf_frames = new string [frame.size];
+	size = frame.size;
+	for(int i=0;i<frame.size;i++){
+		tf_frames[i] = frame.tf_names[i];
+	}
+	ros::Subscriber sub = nh.subscribe ("camera/depth_registered/points", 1, cloud_cb);
+	ros::spin();
+}
 
 
 int main (int argc, char** argv)
@@ -84,10 +114,8 @@ int main (int argc, char** argv)
 	// Create a ROS publisher for the output point cloud
 	pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
 	// Create a ROS subscriber for the input point cloud
-	ros::Subscriber sub = nh.subscribe ("camera/depth_registered/points", 1, cloud_cb);
-
-	// Create a ROS publisher for the output point cloud
-
+	
+	ros::Subscriber sub = nh.subscribe ("coke_tf", 1, frames);
 	// Spin
 	ros::spin ();
 } 
